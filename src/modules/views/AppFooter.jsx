00dddd6be0ad3137d0c4bd9be6918058a49541c4
Hemant Kumar Gothwal <hemant.gothwal1@bp.com>
2024-03-25 02:41:54 +0000

    fixing the footer

--- src/modules/views/AppFooter.jsx
+++ src/modules/views/AppFooter.jsx
@@ -6,6 +6,7 @@ import Typography from "../components/Typography";
 import TextField from "../components/TextField";
 import WhatsAppp from "../../assets/WhatsAppButtonGreenSmall.svg";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import fssai from "../../assets/OIP.jpg";
 
 function Copyright() {
   return (
@@ -47,7 +48,13 @@ export default function AppFooter() {
   return (
     <Box
       component="footer"
-      sx={{ display: "flex", bgcolor: "secondary.light", pt: 6, pb: 6 }}
+      sx={{
+        display: "flex",
+        mt: 6,
+        pt: 6,
+        pb: 6,
+        borderTop: "1px solid lightgrey",
+      }}
     >
       <Container>
         <Grid width="100%" container spacing={4} pt={2} pb={6}>
@@ -86,6 +93,10 @@ export default function AppFooter() {
         <Box display="flex" justifyContent="space-between" pt={2}>
           <Box>
             <Copyright />
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <img alt="fssai" src={fssai} height={64} />
+            <Typography>Lic No. 21424850001581</Typography>
           </Box>
           <Box display="flex" spacing={2} alignItems="center">
             <Box component="a" href="https://mui.com/">
